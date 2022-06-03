cea8bb4df20c22f7f9ef0376a6056f1f6cf465cd
Miguel <vivasmiguel96@gmail.com>
2022-06-02 23:01:14 -0500

    Agregando servicio coingecko

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,8 +48,21 @@ export class AppComponent implements OnInit {
 
 
   ngOnInit(): void {
-    this.http.get<Coin[]>('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-    .subscribe( 
+    // this.http.get<Coin[]>('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    // .subscribe( 
+    //   response => {
+    //   console.log(response);
+    //   this.coins = response;
+    //   this.filteredCoins = response;
+    // },
+    // (err) => console.log(err)
+    // )
+    this.obtenerMonedas();
+   // throw new Error('Method not implemented.');
+  }
+
+  public obtenerMonedas(){
+    this.coinService.getCoins().subscribe( 
       response => {
       console.log(response);
       this.coins = response;
@@ -57,7 +70,8 @@ export class AppComponent implements OnInit {
     },
     (err) => console.log(err)
     )
-   // throw new Error('Method not implemented.');
+
+    
   }
 
 }
--- src/app/services/coin.service.ts
+++ src/app/services/coin.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { observable } from 'rxjs';
+import { Observable, observable } from 'rxjs';
 import { Coin } from '../Coin';
 
 
@@ -13,8 +13,10 @@ export class CoinService {
   constructor(private http:HttpClient) { }
 
 
-  public getCoins(){
+  public getCoins(): Observable<Coin[]>{
 
-    
+  return this.http.get<Coin[]>(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`);
+   
   }
+
 }
